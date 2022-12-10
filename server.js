5d4f4121b442843cd04023d6e6983be95d961414
Prabhakar <prabhakar9169@gmail.com>
2022-12-10 17:01:45 +0530

    server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import app from "./app.js";
 import mongoose from 'mongoose'
 import WebSocket , {WebSocketServer} from 'ws'
 import http from 'http'
+import { v4 as uuidv4 } from 'uuid';
+import url from 'url';
 
 
 const port = process.env.PORT || 3000
@@ -19,12 +21,28 @@ mongoose.connect(dbConnection).then(conn => {
 const server = http.createServer(app)
 
 const wss = new WebSocketServer({server})
-wss.on('connection' , function(ws){
-    console.log('socket Open');
+wss.on('connection' , function(ws, req){
+    ws.id = uuidv4();
+    console.log('socket Open ' , url.parse(req.url , true ).query['key']);
+    const parameters = url.parse(req.url , true ).query
+    if(parameters?.chatroom !== undefined){
+        ws.chatroom = parameters.chatroom
+    }
     ws.on('message' , function(data , isBinary){
+        let chatroomActive = false 
+        if(ws.chatroom !== undefined){
+            chatroomActive = true
+        }
        wss.clients.forEach(function(client){
+
         if(client!== ws && client.readyState === WebSocket.OPEN){
-            client.send(data , {binary:isBinary})
+            if(chatroomActive === false)
+                client.send(data , {binary:isBinary})
+            else{
+                if(ws.chatroom === client.chatroom){
+                    client.send(data , {binary:isBinary})
+                }
+            }
         }
        })
     })
